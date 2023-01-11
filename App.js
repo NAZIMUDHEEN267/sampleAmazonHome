ca5fbb032c926a4ccb5a68855c54615e64bfac6b
nazimudheen267 <nazimudheen267@gmail.com>
2023-01-11 11:22:28 +0530

    project completed

--- App.js
+++ App.js
@@ -2,15 +2,11 @@ import { ScrollView, StyleSheet, View } from 'react-native';
 import Nav from './src/Screens/Nav/Nav';
 import Body from './src/Screens/Body/Body';
 import Footer from './src/Screens/Footer/Footer';
-import { NavigationContainer } from "@react-navigation/native"
-import StackNavigator from './src/Navigators/StackNavigator';
 import { useState } from 'react';
 
 const App = () => {
 
   const [ top, setTop ] = useState("60%");
-
-  console.log(top);
 
   const callback = (value) => {
     setTop(value);
